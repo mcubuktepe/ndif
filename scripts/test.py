56bdf0a11482aa81ad48a32f4aaab0bd0c637ba7
Jaden Fiotto-Kaufman <jadenfk@outlook.com>
2024-07-12 10:50:51 -0400

    updates

--- scripts/test.py
+++ scripts/test.py
@@ -1,11 +1,32 @@
 
 import nnsight
-
-model = nnsight.LanguageModel("meta-llama/Meta-Llama-3-8B")
-
 nnsight.CONFIG.set_default_api_key('0Bb6oUQxj2TuPtlrTkny')
+
+
+model = nnsight.LanguageModel("meta-llama/Meta-Llama-3-70B")
+
 
 with model.trace("ayy", remote=True):
     
     output = model.output.save()
+model = nnsight.LanguageModel("meta-llama/Meta-Llama-3-8B")
+
+
+with model.trace("ayy", remote=True):
+    
+    output = model.output.save()
+    
+model = nnsight.LanguageModel("EleutherAI/gpt-j-6b")
+
+
+with model.trace("ayy", remote=True):
+    
+    output = model.output.save()
+    
+# model = nnsight.LanguageModel("openai-community/gpt2")
+
+
+# with model.trace("ayy", remote=True):
+    
+#     output = model.output.save()
     
--- services/ray_head/start.sh
+++ services/ray_head/start.sh
@@ -8,9 +8,8 @@ ray start --head \
     --object-manager-port=8076 \
     --include-dashboard=true \
     --dashboard-host=0.0.0.0 \
-    --dashboard-port=8265
-    # --system-config "{\"num_heartbeats_timeout\":300000000000}"
-
+    --dashboard-port=8265 
+    
 serve deploy src/ray/config/ray_config.yml
 
 tail -f /dev/null
